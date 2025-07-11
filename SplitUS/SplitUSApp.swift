68e960ae65d266a194ad990d0f589e9342bacded
BWZ-CODE <mmige24@gmail.com>
2025-07-11 13:21:22 -0500

    Initial Commit

---
+++ SplitUS/SplitUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitUSApp.swift
+//  SplitUS
+//
+//  Created by Miguel Angel Bohorquez on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SplitUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
